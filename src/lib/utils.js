a5c6541e45a27f58f2bc4e27060b3d4003920cda
Jquiceno <loncuster@gmail.com>
2021-06-28 12:01:48 +0200

    New package csvtojson added to manage csv files

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,22 +1,9 @@
 'use strict'
 
-function csvToObject (csvText) {
-  let [headers, ...lines] = csvText.trim().split('\n')
-  headers = headers.split(';')
+const csv = require('csvtojson')
 
-  return lines.map(line => {
-    const values = line.split(';')
-
-    const item = {}
-
-    values.map((value, index) => {
-      value = value.slice(0, -1).slice(1)
-      item[headers[index]] = value
-      return true
-    })
-
-    return item
-  })
+async function csvToObject (csvText) {
+  return await csv().fromString(csvText)
 }
 
 module.exports = {
